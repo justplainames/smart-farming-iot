629d5b52bcfe53c26a8a64565200c41da81e3cea
yxxnlxn <55220134+yxxnlxn@users.noreply.github.com>
2022-04-01 20:01:51 +0800

    can control from thingsboard now yay :_

--- thingsboard.py
+++ thingsboard.py
@@ -5,10 +5,25 @@ import paho.mqtt.client as mqtt
 import json
 from sense_hat import SenseHat
 
+sense = SenseHat()
+
+
+def on_message(client, userdata, msg):
+    print('Topic: ' + msg.topic + '\nMessage: ' + str(msg.payload))
+    y = msg.payload
+    x = json.loads(y)
+    print(x['params'])
+    if (str(x['params']) == "False"):
+    	sense.clear((0,0,0))
+    elif (str(x['params']) == "True"):
+        sense.clear((255,0,0))
+
+
+
 THINGSBOARD_HOST = 'demo.thingsboard.io'
 ACCESS_TOKEN = 'y3SmMaulErsDoe823pjQ'
 
-INTERVAL=2
+INTERVAL = 5
 
 sense = SenseHat()
 sensor_data = {'temperature': 0, 'humidity': 0}
@@ -23,6 +38,7 @@ client.username_pw_set(ACCESS_TOKEN)
 # Connect to ThingsBoard using default MQTT port and 60 seconds keepalive interval
 client.connect(THINGSBOARD_HOST, 1883, 60)
 
+
 client.loop_start()
 
 try:
@@ -31,12 +47,15 @@ try:
         temperature = sense.get_temperature()
         humidity = round(humidity, 2)
         temperature = round(temperature, 2)
-        print(u"Temperature: {:g}, Humidity: {:g}%".format(temperature, humidity))
+        print(u"Temperature: {:g}, Humidity: {:g}%".format(
+            temperature, humidity))
         sensor_data['temperature'] = temperature
         sensor_data['humidity'] = humidity
 
         # Sending humidity and temperature data to ThingsBoard
         client.publish('v1/devices/me/telemetry', json.dumps(sensor_data), 1)
+        client.subscribe('v1/devices/me/rpc/request/+')
+        client.on_message = on_message
 
         next_reading += INTERVAL
         sleep_time = next_reading-time.time()
@@ -46,4 +65,4 @@ except KeyboardInterrupt:
     pass
 
 client.loop_stop()
-client.disconnect() 
+client.disconnect()
