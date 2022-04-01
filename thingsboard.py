cb94c9ada8b245f878f82c7f2465cba6c7eb15fa
yxxnlxn <55220134+yxxnlxn@users.noreply.github.com>
2022-04-01 14:02:57 +0800

    transfer data to thingsboard done

---
+++ thingsboard.py
@@ -0,0 +1,49 @@
+import os
+import time
+import sys
+import paho.mqtt.client as mqtt
+import json
+from sense_hat import SenseHat
+
+THINGSBOARD_HOST = 'demo.thingsboard.io'
+ACCESS_TOKEN = 'y3SmMaulErsDoe823pjQ'
+
+INTERVAL=2
+
+sense = SenseHat()
+sensor_data = {'temperature': 0, 'humidity': 0}
+
+next_reading = time.time()
+
+client = mqtt.Client()
+
+# Set access token
+client.username_pw_set(ACCESS_TOKEN)
+
+# Connect to ThingsBoard using default MQTT port and 60 seconds keepalive interval
+client.connect(THINGSBOARD_HOST, 1883, 60)
+
+client.loop_start()
+
+try:
+    while True:
+        humidity = sense.get_humidity()
+        temperature = sense.get_temperature()
+        humidity = round(humidity, 2)
+        temperature = round(temperature, 2)
+        print(u"Temperature: {:g}, Humidity: {:g}%".format(temperature, humidity))
+        sensor_data['temperature'] = temperature
+        sensor_data['humidity'] = humidity
+
+        # Sending humidity and temperature data to ThingsBoard
+        client.publish('v1/devices/me/telemetry', json.dumps(sensor_data), 1)
+
+        next_reading += INTERVAL
+        sleep_time = next_reading-time.time()
+        if sleep_time > 0:
+            time.sleep(sleep_time)
+except KeyboardInterrupt:
+    pass
+
+client.loop_stop()
+client.disconnect() 
